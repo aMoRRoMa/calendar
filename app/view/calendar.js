4cb98927b06bfc34a3f012341fe8ff6df54fa11f
aMoRRoMa <gafiatullin.roman@gmail.com>
2013-03-23 19:08:22 +0400

    calendar

--- app/view/calendar.js
+++ app/view/calendar.js
@@ -3,7 +3,11 @@ define([
   'common'
 ], function (Backbone, Common) {
   var Calendar = Backbone.View.extend({
-
+    el:'#calendar',
+    initialize:function(){
+      var today = new Date()
+      console.log(today.getDate())
+    }
   });
   return Calendar
 });
